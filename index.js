525d6c9ba8a37dce3ce0999bc4cd50a7f03ed12c
mishasmcd1 <msm1@uchicago.edu>
2022-02-25 13:36:14 -0600

    commit tree 7262ba13be74514002a59470facb0bcadda8131e

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+  return "This one is on me!"
+  } else if (someValue > 2500) {
+  return "No can do."
+} else if (someValue >= 2000) {
+  return "I will gladly take your thirty bucks.";
+}}
+function ternaryCheckCity(city){
+return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous' :
+      return "Thank you so much."; 
+      break;
+    case 'not as generous' :
+     return  "Thank you.";
+      break;
+    default :
+      return "Bye.";
+      break;
+  }}
